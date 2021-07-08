23348e63a27c4de23139fb3ef73d710091fa3655
emmateva18 <emmateva18@codingburgas.bg>
2021-07-08 23:59:54 +0300

    Minor changes:
    * Add comments
    * Delete repeating lines

--- Converter/ClassLibraryProjects/ShowCase/Program.cs
+++ Converter/ClassLibraryProjects/ShowCase/Program.cs
@@ -17,6 +17,7 @@ namespace ConverterLibrary
             // Set delimiter
             string delimiter = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
 
+            // Add example data
             List<Users> users = new List<Users>();
             users.Add(new Users() { FirstName = "FN1" });
             users.Add(new Users() { FirstName = "FN2" });
@@ -26,12 +27,10 @@ namespace ConverterLibrary
             bikes.Add(new Bike() { Name = "Bike2", Model = "Model 2", Year = 2021 });
             bikes.Add(new Bike() { Name = "Bike3", Model = "Model 3", Year = 2019 });
 
-            string sep = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ListSeparator;
-
-            ExportData.ListToCsv(bikes,path, sep);
+            // Call function for exporting data
+            ExportData.ListToCsv(bikes,path, delimiter);
 
             //DataTable dtProducts = ImportData.GetProducts();
-
             //ExportData.ToCSVWithFile(dtProducts, productsFilePath);
         }
     }
